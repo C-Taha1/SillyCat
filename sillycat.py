aab9ca66780d3f36e9ea22da0c8375f215b91ac9
C-Taha1 <ctaha@proton.me>
2026-01-05 03:47:51 -0600

    [4/1/2026]: refactored sillycat.py

---
+++ sillycat.py
@@ -0,0 +1,115 @@
+import os , sys
+
+# GLOBAL VARS
+TOOLS: list = os.listdir("scripts")
+RUNNERS: list = ["python3"] # intrepeters / compilers
+COMMANDS = {
+        
+            "help"  : "show this help menu", 
+            "clear" : "clear the screen",
+            "list"  : "list all scripts",
+            "load"  : "load a file ( DOESNT ACCEPT ARGS )"
+        }
+
+
+# UI compenents 
+def clear() -> None:
+    os.system("cls" if os.name == "nt" else "clear")
+
+ 
+def list_files() -> None:
+     for (num , file) in enumerate(TOOLS):
+         print(f"\t [*] {num} :: {file}")
+
+
+         
+def logo() -> None:
+    art = r"""  ⠀⠀⠀
+   ⢠⣾⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣰⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢰⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⣀⣀⣤⣤⣶⣾⣿⣿⣿⡷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀
+⣿⣿⣿⡇⠀⡾⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀
+⣿⣿⣿⣧⡀⠁⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢹⠉⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⣀⣼⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠀⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⠿⠋⢃⠈⠢⡁⠒⠄⡀⠈⠁⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⠟⠁⠀⠀⠈⠉⠉⠁⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        Do you like cats?
+
+"""
+
+    # typewritter effect ( dramatic )
+    from time import sleep
+    for char in art:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        sleep(0.002)
+
+def help_menu() -> None:
+    for (KEY , VALUE) in COMMANDS.items():
+        print(f"\t [+] {KEY} -- {VALUE}")
+
+
+
+# Main CLI
+def cmd_line():
+    
+    # visuals
+    clear()
+    logo()
+
+
+    while True:
+        cmd = input("silly > ").lower()
+
+        if cmd == "exit":
+            print(f"Have Great Hacking Day...")
+            break
+
+
+        elif cmd == "help":
+            help_menu()
+
+        elif cmd == "list":
+            list_files()
+        
+        elif cmd  == "clear":
+            clear()
+
+        elif cmd == "load":
+            choose_and_run()
+
+
+# The 'load' Command Logic   ( NOTE : Sorry i put it as the last , im still a noob at neovim and idk how to cut and past , thanks)      
+def choose_and_run():
+    
+    try:
+    
+        choice = int(input("[file index] :: "))
+
+        choosen_file = TOOLS[choice]
+
+        if choosen_file.endswith(".py"):
+            os.system(f"{RUNNERS[0]} scripts/{choosen_file}")
+        
+        else: print(f"[Error] :: Cant Find File '{choosen_file}'")
+
+    
+    except Exception as exp:
+        print(f"[Error] :: {exp}")
+
+
+
+if __name__ == "__main__":
+    cmd_line()
